c4a0fc562d19c4b316842e87cb4586ef78d49b43
larryhwang <FengHua@chenkaiquandemac-mini.local>
2016-03-23 10:16:26 +0800

    Initial Commit

---
+++ xingdian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xingdian
+//
+//  Created by FengHua on 3/23/16.
+//  Copyright © 2016 PeaK Points Tec Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xingdian/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xingdian
+//
+//  Created by FengHua on 3/23/16.
+//  Copyright © 2016 PeaK Points Tec Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
